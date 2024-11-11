102ec58348ea669a5a1bc73b6b3c6d7b2f2b9a22
BrunoFonttesJ <bruno.fontes@jackinthebox.com>
2024-11-10 21:34:29 -0300

    implements graceful shutdown

--- server.js
+++ server.js
@@ -4,10 +4,30 @@ const PORT = 3000
 
 app.use(express.json())
 
-app.get("/health", (req,res)=>{
-    res.json({message:"hello world"})
+app.get("/health", (req, res) => {
+    res.json({ message: "hello world" })
 })
 
-app.listen(PORT,()=>{
+app.get("/heavy-task", (req, res) => {
+    setTimeout(() => {
+        res.json({ message: "heavy task completed!" })
+    }, (10000));
+})
+
+const server = app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 })
+
+process.on("SIGINT", gracefulShutdownHandler) // ctrl + c singal
+process.on("SIGTERM", gracefulShutdownHandler) // docker system level termination signal
+
+function gracefulShutdownHandler(signal) {
+    console.info(`caught ${signal}, gracefully shutting down`);
+
+    server.close(() => {
+        console.info("all requests stopped, shutting down")
+        // Perform cleanup tasks here
+        // i.e: close database connections 
+        process.exit()
+    })
+}
